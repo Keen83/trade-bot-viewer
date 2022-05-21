5d06ffa5d09610cdc9185698260b0cb2d36bc17e
Voffka <volodymyr.nik@gmail.com>
2022-05-21 19:55:52 +0300

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Instruments } from './components/Instruments/Instruments';
+import { NewInstrument } from './components/Instruments/NewInstrument/NewInstrument';
+import { EditInstrument } from './components/Instruments/Instrument/EditInstrument/EditInstrument';
+import { Chart } from './components/Instruments/Instrument/ChartInstrument/Chart';
+import { Login } from './components/Login/Login';
+import { Profile } from './components/Profile/Profile';
+import { Home } from './components/Home/Home';
+import { Instrument } from './components/Instruments/Instrument/Instrument';
 
 function App() {
   return (
-    <div className="app">
-      <header className='header' ></header>
-      <nav className='nav' ></nav>
-      <main className='content' ></main>
-    </div>
+    <BrowserRouter>
+      <div className="app">       
+          <Routes>
+            <Route path='login' element={<Login />} />
+            <Route path='/' element={<Home />}>
+              <Route path='profile' element={<Profile />} />
+              <Route path='instruments' element={<Instruments />}>
+                <Route path='new' element={<NewInstrument />} />
+                <Route path=':id' element={<Instrument />} >
+                  <Route path='edit' element={<EditInstrument />} />
+                  <Route path='chart' element={<Chart />} />
+                </Route>
+              </Route>
+            </Route>
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
