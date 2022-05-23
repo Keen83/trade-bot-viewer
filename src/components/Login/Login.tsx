18155eaaf72a38ad24be0f33e4cacf6494c39a2b
Olexandr Nikolaevsky <olexandr.nikolaevsky@if.no>
2022-05-23 10:56:42 +0300

    Add google login button

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import GoogleSignin from "../Auth/GoogleSignin/GoogleSignin";
+
+import styles from './Login.module.css'
 
 export function Login() {
     return(
-        <div>
-            Login
+        <div className={styles.loginContainer}>
+          <div className="login-header">Login with:</div>
+          <div className="login-items">
+            <GoogleSignin/>
+          </div>
         </div>
     )
 }
