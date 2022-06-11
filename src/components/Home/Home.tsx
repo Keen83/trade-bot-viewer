a6a9dc37e8b6c5b3fa7dc299a6cd06fac6b81d26
Voffka <volodymyr.nik@gmail.com>
2022-06-11 15:19:03 +0300

    Add auth to the reduser

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useStore } from "react-redux";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 export function Home() {
     return (
